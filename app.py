84c4f125e2c8bc2bbd6218b491bd080d770d8cef
Natetodd55 <nathantodd55@gmail.com>
2024-04-29 11:55:26 -0600

    added staff

--- app.py
+++ app.py
@@ -64,28 +64,67 @@ class Benefit(db.Model):
     membership_id = db.Column(db.Integer, db.ForeignKey('memberships.id'), nullable=False)
     services = db.relationship('Service', backref='benefit')
 
+# class Service(db.Model):
+#     __tablename__ = 'services'
+#
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(30), nullable=False)
+#     time = db.Column(db.String(5), nullable=False)
+#     date = db.Column(db.String(10), nullable=False)
+#     instructor = db.Column(db.String(50), nullable=False)
+#     benefit_id = db.Column(db.Integer, db.ForeignKey('benefits.id'), nullable=False)
+class Staff(UserMixin, db.Model):
+    __tablename__ = 'staff'
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(20), nullable=False)
+    last_name = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(75), nullable=False)
+    phone = db.Column(db.String(10))
+    authenticated = db.Column(db.Boolean, default=False, nullable=False)
+    services = db.relationship('Service', backref='instructor', lazy='dynamic')
+
+    def is_authenticated(self):
+        return self.authenticated
+
+    def is_active(self):
+        return True
+
+    def get_id(self):
+        return self.id
+
+    def is_anonymous(self):
+        return False
+
 class Service(db.Model):
     __tablename__ = 'services'
-
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), nullable=False)
     time = db.Column(db.String(5), nullable=False)
     date = db.Column(db.String(10), nullable=False)
-    instructor = db.Column(db.String(50), nullable=False)
+    instructor_id = db.Column(db.Integer, db.ForeignKey('staff.id'), nullable=False)
     benefit_id = db.Column(db.Integer, db.ForeignKey('benefits.id'), nullable=False)
 
 
 
 
 ######################### Helper_Functions ###################################
-def get_member_services(member):
+def benefits_from_member(member):
     print(f"Member found: {member.first_name} {member.last_name} (ID: {member.id})")
-    services = []
+    benefits = None
     membership = member.membership
     if membership:
         print("membership ID: ", membership.id)
         benefits = membership.benefits
-        print("Benefits attached to membership: ", benefits)
+
+    return benefits
+
+
+def get_all_member_services(member):
+    # print(f"Member found: {member.first_name} {member.last_name} (ID: {member.id})")
+    services = []
+    benefits = benefits_from_member(member)
+    if benefits:
         for benefit in benefits:
             print(f"Benefit(ID={benefit.id}) is {benefit.name}")
             print(f"Benefit services: {benefit.services}")
@@ -97,9 +136,49 @@ def get_member_services(member):
                 if serv.benefit_id == benefit.id:
                     print(f"Service(ID={serv.id}) is attached to {benefit.name}")
                     services.extend(Service.query.filter_by(id=serv.id))
+
     if len(services) == 0:
         return None
     return services
+
+
+def get_all_services_from_member_benefits(member):
+    print(f"Member found: {member.first_name} {member.last_name} (ID: {member.id})")
+    services = []
+    benefits = benefits_from_member(member)
+
+    if benefits:
+        for benefit in benefits:
+            services_attached = Service.query.filter_by(name = benefit.name).all()
+            print(f"Services attached to benefit({benefit.name}): {services_attached}")
+        # for benefit in benefits:
+        #     print(f"Benefit(ID={benefit.id}) is {benefit.name}")
+        #     print(f"Benefit services: {benefit.services}")
+        #     servs = Service.query.all()
+        #     print(f"Services:  {servs}")
+        #     for serv in servs:
+        #         print(f"Service(ID={serv.id}) is {serv.name} with benefit_id = {serv.benefit_id} associated to Benefit(ID={benefit.id})")
+        #         print(f"Service.benefit_id({serv.benefit_id}) == benefit.id({benefit.id})  -> {serv.benefit_id == benefit.id}")
+        #         if serv.benefit_id == benefit.id:
+        #             print(f"Service(ID={serv.id}) is attached to {benefit.name}")
+        #             services.extend(Service.query.filter_by(id=serv.id))
+
+    if len(services) == 0:
+        return None
+    return services
+
+
+def find_login_from_email(email):
+    member = Member.query.filter_by(email=email).first()
+    staff = Staff.query.filter_by(email=email).first()
+    if member:
+        return member
+    elif staff:
+        return staff
+    else:
+        return None
+
+
 
 @lm.user_loader
 def load_user(uid):
@@ -111,19 +190,21 @@ def load_user(uid):
 ############################ flask_routes ###########################
 @app.route("/")
 def home():
-    return render_template("dashboard.html", outline="outline1.html")
+
+    return render_template("dashboard.html")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if (request.method=="POST"):
         email = request.form['email']
-        member = Member.query.filter_by(email=email).first()
-        if member:
-            if request.form['password'] == cipher.decrypt(member.password).decode('utf-8'):
-                member.authenticated = True
-                db.session.add(member)
+        temp_login = find_login_from_email(email)
+        # member = Member.query.filter_by(email=email).first()
+        if temp_login: #member
+            if request.form['password'] == cipher.decrypt(temp_login.password).decode('utf-8'):
+                temp_login.authenticated = True
+                db.session.add(temp_login)
                 db.session.commit()
-                login_user(member, remember=True)
+                login_user(temp_login, remember=True)
                 return render_template('dashboard.html')
 
     return render_template('login.html')
@@ -168,9 +249,17 @@ def membership():
 
 @app.route("/training")
 def training():
-    services = get_member_services(current_user)
+    get_all_services_from_member_benefits(current_user)
+    #all_services = get_all_member_services(current_user)
+    return  render_template("training.html")
+    # return render_template("training.html", services=all_services)
 
-    return render_template("training.html", services=services)
+@app.route("/add_services", methods = ["GET", "POST"])
+def add_services():
+    if
+
+    return render_template("add_services.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
